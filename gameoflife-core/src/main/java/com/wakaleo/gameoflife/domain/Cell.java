f19b61bdc3db5ef606b9deb32c441062bd93dfa5
leo wang <wanglei830210@gmail.com>
2017-01-20 10:14:35 +0800

    restore

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,11 +1,10 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    //LIVE_CELL("*"), DEAD_CELL(".");
-    LIVE_CELL("-"), DEAD_CELL(".");
+    LIVE_CELL("*"), DEAD_CELL(".");
+
     private String symbol;
-    private String symbol1;
-    private String symbol2;
+
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
